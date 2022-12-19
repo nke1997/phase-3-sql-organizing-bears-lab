58755f119e822e3ee3ae73be8a05e18bbb95d6b3
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-12-19 11:27:23 -0500

    commit tree 5c21e95b8ec435f8449f330a5ab9cabc665f2fc8

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 8, "M", "brown", "docile", 1),
+("Rowdy", 2, "M", "black", "aggressive", 1),
+("Tabithat", 9, "F", "brown", "docile", 0),
+("Sergeant Brown", 11, "M", "brown", "aggressive", 0),
+("Melissa", 4, "F", "black", "aggressive", 1),
+("Grinch", 3, "M", "white", "docile", 1),
+("Wendy", 5, "F", "black", "aggressive", 1),
+(null, 1, "F", "white", "docile", 1)
